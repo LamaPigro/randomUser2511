074da58077936131ad3e6b93a9e8d542f38b8436
Marco Vaccaro <97899318+LamaPigro@users.noreply.github.com>
2022-11-25 07:29:10 +0000

    adding async fuction

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
 <script>
- 
+  async function GetUserProfile() {
+    const response = await fetch("https://randomuser.me/api/")
+
+    // verifica controllo dati
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    } else {
+      throw new Error("Errore nella fetch del profilo!!")
+    } 
+  }
 </script>
 
 <main>
